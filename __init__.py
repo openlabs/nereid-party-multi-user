d7345f3d7cca22ac8c579431674dbbe7dd46ec46
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-04-27 11:25:30 +0530

    Migrato to 2.6 active record pattern #1129
    * Display name field has been removed since it now exists in core of
      nereid.

--- __init__.py
+++ __init__.py
@@ -5,4 +5,13 @@
     :copyright: (c) 2012 by Openlabs Technologies & Consulting (P) LTD
     :license: GPLv3, see LICENSE for more details.
 """
-from user import *
+from trytond.pool import Pool
+from .user import NereidUser, NereidUserParty, Party
+
+
+def register():
+    Pool.register(
+        NereidUser,
+        NereidUserParty,
+        Party,
+        module='nereid_party_multi_user', type_='model')
